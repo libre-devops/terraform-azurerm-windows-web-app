e1e4d7d95b629d8c9c262cb0156b6d30caeeeac2
Craig Thacker <craigthackerx@gmail.com>
2022-05-31 18:38:01 +0100

    Update module

--- terraform/build.tf
+++ terraform/build.tf
@@ -8,54 +8,7 @@ module "rg" {
   #  lock_level = "CanNotDelete" // Do not set this value to skip lock
 }
 
-// This module does not consider for CMKs and allows the users to manually set bypasses
-#checkov:skip=CKV2_AZURE_1:CMKs are not considered in this module
-#checkov:skip=CKV2_AZURE_18:CMKs are not considered in this module
-#checkov:skip=CKV_AZURE_33:Storage logging is not configured by default in this module
-#tfsec:ignore:azure-storage-queue-services-logging-enabled tfsec:ignore:azure-storage-allow-microsoft-service-bypass #tfsec:ignore:azure-storage-default-action-deny
-module "sa" {
-  source = "registry.terraform.io/libre-devops/storage-account/azurerm"
-
-  rg_name  = module.rg.rg_name
-  location = module.rg.rg_location
-  tags     = module.rg.rg_tags
-
-  storage_account_name            = "st${var.short}${var.loc}${terraform.workspace}01"
-  access_tier                     = "Hot"
-  identity_type                   = "SystemAssigned"
-  allow_nested_items_to_be_public = true
-
-  storage_account_properties = {
-
-    // Set this block to enable network rules
-    network_rules = {
-      default_action = "Allow"
-    }
-
-    blob_properties = {
-      versioning_enabled       = false
-      change_feed_enabled      = false
-      default_service_version  = "2020-06-12"
-      last_access_time_enabled = false
-
-      deletion_retention_policies = {
-        days = 10
-      }
-
-      container_delete_retention_policy = {
-        days = 10
-      }
-    }
-
-    routing = {
-      publish_internet_endpoints  = false
-      publish_microsoft_endpoints = true
-      choice                      = "MicrosoftRouting"
-    }
-  }
-}
-
-module "fnc_plan" {
+module "plan" {
   source = "registry.terraform.io/libre-devops/service-plan/azurerm"
 
   rg_name  = module.rg.rg_name
@@ -66,26 +19,23 @@ module "fnc_plan" {
   add_to_app_service_environment = false
 
   os_type  = "Windows"
-  sku_name = "Y1"
+  sku_name = "S1"
 }
 
 #checkov:skip=CKV2_AZURE_145:TLS 1.2 is allegedly the latest supported as per hashicorp docs
-module "fnc_app" {
-  source = "registry.terraform.io/libre-devops/windows-function-app/azurerm"
+module "web_app" {
+  source = "registry.terraform.io/libre-devops/windows-web-app/azurerm"
 
   rg_name  = module.rg.rg_name
   location = module.rg.rg_location
   tags     = module.rg.rg_tags
 
-  app_name        = "fnc-${var.short}-${var.loc}-${terraform.workspace}-01"
-  service_plan_id = module.fnc_plan.service_plan_id
+  app_name        = "app-${var.short}-${var.loc}-${terraform.workspace}-01"
+  service_plan_id = module.plan.service_plan_id
 
-  storage_account_name          = module.sa.sa_name
-  storage_account_access_key    = module.sa.sa_primary_access_key
   storage_uses_managed_identity = "false"
 
-  identity_type               = "SystemAssigned"
-  functions_extension_version = "~4"
+  identity_type = "SystemAssigned"
 
   settings = {
     site_config = {
@@ -93,7 +43,7 @@ module "fnc_app" {
       http2_enabled       = true
 
       application_stack = {
-        powershell_core_version = 7
+        python_version = 3.9
       }
     }
 
